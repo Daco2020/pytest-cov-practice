069b288a796035ab666cc75bea32a34a1b4ad6c3
Daco2020 <dacokim32@gmail.com>
2022-09-05 20:24:36 +0900

    example setting

---
+++ main/file.py
@@ -0,0 +1,9 @@
+true_value = "나는 올바른 값이야"
+false_value = "나는 잘못된 값이야"
+
+
+def get_value(bool):
+    if bool:
+        return true_value
+    else:
+        return false_value
---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from main.file import get_value
+
+
+def test_get_true_value():
+    assert get_value(True) == "나는 올바른 값이야"
+
+
+def test_get_false_value():
+    assert get_value(False) == "나는 잘못된 값이야"
